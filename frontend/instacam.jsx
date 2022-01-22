2b4123239bd2f5d0bd9ec6dd311474d8ecccc6bb
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-21 20:07:56 -0800

    Finishing React

--- frontend/instacam.jsx
+++ frontend/instacam.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
+import Root from './components/root';
 
 // import { signup, login, logout } from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const root = document.getElementById("root");
     const store = configureStore();
 
+    // TESTING START
     // window.signup = signup;
     // window.login = login;
     // window.logout = logout;
 
     window.store = store;
 
-    ReactDOM.render(<h1>Welcome to Instacam!</h1>, root);
+    // TESTING END
+    
+    const root = document.getElementById("root");
+    ReactDOM.render(<Root store={store} />, root);
 });
