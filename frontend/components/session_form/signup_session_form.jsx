8349cf8fb0800db5d43f65c4242b3fd7aa8beaa6
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-24 15:43:24 -0800

    Fixing phone photo

--- frontend/components/session_form/signup_session_form.jsx
+++ frontend/components/session_form/signup_session_form.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SessionFormPhonePhoto from '../../../app/assets/images/session_form_phone.png'
 
 class SignupSessionForm extends React.Component {
     constructor(props) {
