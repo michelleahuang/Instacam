d3901d68a5cce48d5609693fed2eb97534707158
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-23 21:51:15 -0800

    Add additional error message for login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,11 @@ class Api::SessionsController < ApplicationController
         @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
         
         if @user.nil?
-            render json: ["Invalid username/password combination"], status: 401
+            if params[:user][:username].empty? && params[:user][:password].empty? 
+                render json: ["Username and password fields cannot be blank"], status: 401
+            else 
+                render json: ["Invalid username/password combination"], status: 401
+            end
         else  
             login!(@user)
             render "api/users/show"
