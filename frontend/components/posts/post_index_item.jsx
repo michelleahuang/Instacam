aaf44b2fa4307415d71b2f429ba49e6316f9e619
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-26 09:47:11 -0800

    Profile pictures and username shows on index page

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -11,8 +11,8 @@ class PostIndexItem extends React.Component {
                 <li>
                     <div id="post-header">
                         <div id="post-header-left">
-                            <div className="post-profile-icon"></div>
-                            <p id="post-username">Username</p>
+                            <img className="post-profile-icon" src={this.props.post.creatorAvatar}></img>
+                            <p id="post-username">{this.props.post.creator}</p>
                         </div>
                         <div id="post-header-right">
                             <svg aria-label="More options" className="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
