0869c6001412f7d4043528241ac0f3ff396313c9
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-26 01:23:49 -0800

    Update class naming

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -15,11 +15,11 @@ class PostIndexItem extends React.Component {
                             <p id="post-username">Username</p>
                         </div>
                         <div id="post-header-right">
-                            <svg aria-label="More options" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
+                            <svg aria-label="More options" className="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
                         </div>
                     </div>
                     <img className="post-photo" src={this.props.post.photoUrl}></img>
-                    
+
                 </li>
             </div>
         );
