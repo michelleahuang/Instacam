803aa3304885a4b641c2188b47fa31dde01faa09
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-26 01:17:24 -0800

    Work on user actions and reducer

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,11 @@ require "open-uri"
 
 class Api::UsersController < ApplicationController
 
+    def index 
+        @users = User.all 
+        render :index
+    end 
+
     def create 
         @user = User.new(user_params)
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do 
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show, :index]
     resources :posts, only: [:index, :create, :show, :destroy, :update]
     resource :session, only: [:create, :destroy]
   end
