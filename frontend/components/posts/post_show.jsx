e182d51bfa04230d668af0b8f7a977ab7cb5bb16
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-26 17:14:18 -0800

    Post Show CSS not working

--- frontend/components/posts/post_show.jsx
+++ frontend/components/posts/post_show.jsx
@@ -15,14 +15,17 @@ class PostShow extends React.Component {
         return (
             <div id="post-show-page">
                 <div id="post-show-container">
+                    <div id="testing">
+
                     <img id="individual-post-photo" src={this.props.post.photoUrl}></img>
+                    </div>
                     <div id="post-photo-right-container">
                         <div id="post-photo-right-side">
                             <div id="right-side-header">
                                 <img className="post-profile-icon" src={this.props.post.creatorAvatar}></img>
-                                    <p id="post-username">{this.props.post.creator}</p>
-                                    <p id="period">•</p>
-                                    <p>Following</p>
+                                <p id="post-username">{this.props.post.creator}</p>
+                                <p id="period">•</p>
+                                <p>Following</p>
                             </div>
                         </div>
 
