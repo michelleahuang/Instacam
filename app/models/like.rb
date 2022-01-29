c3c424258492cf68d51e4ba3dea6d236556f4de6
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-29 00:33:11 -0800

    Create likes and comments migration and model

---
+++ app/models/like.rb
@@ -0,0 +1,16 @@
+class Like < ApplicationRecord
+    validates :user_id, :post_id, presence: true 
+    validates :user_id, uniqueness: {scope: :post_id, message: "User can only like a single post once"}
+
+    belongs_to :user,
+        primary_key: :id, 
+        foreign_key: :user_id,
+        class_name: :User
+
+    
+    belongs_to :post,
+        primary_key: :id,
+        foreign_key: :post_id,
+        class_name: :Post
+
+end
