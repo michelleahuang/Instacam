a8f61c0ce647f9909e7a0c34cafdc4e02f3e4a4d
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-29 01:33:59 -0800

    Finish likes controller

---
+++ app/controllers/api/likes_controller.rb
@@ -0,0 +1,38 @@
+class Api::LikesController < ApplicationController
+
+    def index  
+        @likes = Like.all 
+        render :index 
+    end 
+
+    def create 
+        @like = Like.new(like_params)
+        @like.user_id = current_user.id
+
+        if @like.save 
+            render :show 
+        else  
+            render json: @like.errors.full_messages, status: 422
+        end 
+
+    end 
+
+    def destroy
+        @like = Like.find_by(id: params[:id])
+
+        if @like && (current_user.id == @like.user_id)
+            @like.destroy 
+            render :show 
+        else  
+            render json: @like.errors.full_messages, status: 422
+        end 
+        
+    end 
+
+    private 
+
+    def like_params 
+        params.require(:like).permit(:user_id, :post_id)
+    end 
+
+end
