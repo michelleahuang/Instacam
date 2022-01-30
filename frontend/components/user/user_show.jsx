61c597ed8e03d113f1698891e2218c89b0c7c6d9
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-30 11:59:51 -0800

    Finish styling post photo

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -53,12 +53,12 @@ class UserShow extends React.Component {
                                 <img id="post-photo" src={post.photoUrl}></img>
                                 <div id="post-photo-overlay">
                                     <div id="post-photo-stats">
-                                        <div className="post-photo-stats-numbers">
-                                            <svg aria-label="Unlike" class="_8-yf5 " color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>                                            
+                                        <div id="post-photo-likes-container" className="post-photo-stats-numbers">
+                                            <svg className="post-svg" aria-label="Unlike" color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>                                            
                                             <p>#</p>
                                         </div>
                                         <div className="post-photo-stats-numbers">
-                                            <svg aria-label="Comment" class="_8-yf5 " color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="#FFFFFF" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+                                            <svg className="post-svg" aria-label="Comment" color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="#FFFFFF" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path></svg>
                                             <p>#</p>
                                         </div>
                                     </div>
