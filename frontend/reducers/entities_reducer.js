fa82ed6f8c93c779e6c454ac5da381537e2196f3
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-30 14:46:39 -0800

    Working on comments actions and reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducers';
 import likesReducer from './likes_reducers';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     posts: postsReducer,
-    likes: likesReducer
+    likes: likesReducer,
+    comments: commentsReducer
 });
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import postsErrorsReducer from './posts_errors_reducer';
 import likesErrorsReducer from './likes_errors_reducer';
+import commentsErrorsReducer from './comments_errors_reducer';
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
     post: postsErrorsReducer,
-    like: likesErrorsReducer
+    like: likesErrorsReducer,
+    comment: commentsErrorsReducer
 });
 
 export default errorsReducer;
