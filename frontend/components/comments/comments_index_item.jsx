b878f6cf5a25db8aa716b9062994511cc9f31190
Michelle Huang <michelleahuang@berkeley.edu>
2022-01-30 23:35:34 -0800

    Get trash can icon added

--- frontend/components/comments/comments_index_item.jsx
+++ frontend/components/comments/comments_index_item.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 class CommentsIndexItem extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        if (this.props.comment.userId === this.props.currentUserId) {
+            return (
+                <div>
+                    <i className="fas fa-trash"></i>
+                </div>
+            )
+        } else {
+            return null;
+        }
     }
 
     render() {
@@ -11,6 +25,7 @@ class CommentsIndexItem extends React.Component {
                 <div id="comments-body-item-container">
                     <p id="comment-username">{this.props.comment.username}</p>
                     <p id="comment-body">{this.props.comment.body}</p>
+                    {this.handleDelete()}
                 </div>
             </li>
         )
