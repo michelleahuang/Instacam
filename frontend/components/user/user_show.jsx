b8804830993d2cb34c3531a8d1e71357de9d0da6
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-01 10:25:02 -0800

    Create no post for user profile

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -17,16 +17,49 @@ class UserShow extends React.Component {
     }
 
     render() {
+
         let usersShow;
 
         if (!this.props.user) {
             usersShow = null;
         } else {
+            let display;
             let posts = this.props.user.posts;
 
             let followers = this.props.user.totalCount ? this.props.user.totalCount - 1 : 0;
             let followings = this.props.user.totalCount ?  this.props.user.totalCount - 1 : 0;
 
+
+        if (this.props.user.posts.length === 0) {
+            display = 
+            <div id="no-posts-container">
+                <div id="no-posts-image-container">
+                    <img src="https://img.icons8.com/ios/30/000000/unsplash.png"/>
+                </div>
+                <p id="no-posts-text">No Posts Yet</p>
+            </div>
+        } else {
+            display = 
+            <div id="posts-grid-container">
+                {posts.slice(0).reverse().map((post, index) => 
+                    <div id="individual-post-item" key={index}>
+                        <img id="post-photo" src={post.photoUrl}></img>
+                        <div id="post-photo-overlay">
+                            <div id="post-photo-stats">
+                                <div id="post-photo-likes-container" className="post-photo-stats-numbers">
+                                    <svg className="post-svg" aria-label="Unlike" color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>                                            
+                                    <p>{post.likes.length}</p>
+                                </div>
+                                <div className="post-photo-stats-numbers">
+                                    <svg className="post-svg" aria-label="Comment" color="#FFFFFF" fill="#FFFFFF" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="#FFFFFF" stroke="currentColor" strokeLinejoin="round" strokeWidth="2"></path></svg>
+                                    <p>{post.comments.length}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        }
             usersShow = 
             <div id="user-show-page">
                 <div id="user-show-header">
@@ -47,7 +80,8 @@ class UserShow extends React.Component {
                     <svg id="posts-divider-icon" aria-label="" className="_8-yf5 " color="#262626" fill="#262626" height="12" role="img" viewBox="0 0 24 24" width="12"><rect fill="none" height="18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="18" x="3" y="3"></rect><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="9.015" x2="9.015" y1="3" y2="21"></line><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="14.985" x2="14.985" y1="3" y2="21"></line><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="21" x2="3" y1="9.015" y2="9.015"></line><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="21" x2="3" y1="14.985" y2="14.985"></line></svg>
                     <p id="posts-divider-text">POSTS</p>
                 </div>
-                <div id="posts-grid-container">
+                {display}
+                {/* <div id="posts-grid-container">
                         {posts.slice(0).reverse().map((post, index) => 
                             <div id="individual-post-item" key={index}>
                                 <img id="post-photo" src={post.photoUrl}></img>
@@ -65,7 +99,7 @@ class UserShow extends React.Component {
                                 </div>
                             </div>
                         )}
-                </div>
+                </div> */}
             </div>
         }
         
