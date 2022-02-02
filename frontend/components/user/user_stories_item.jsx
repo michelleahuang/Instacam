d3f6499f65675b8b855b2568d95a01b64c644568
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-02 12:56:39 -0800

    Fix right side positioning

--- frontend/components/user/user_stories_item.jsx
+++ frontend/components/user/user_stories_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class UserStoriesItem extends React.Component {
     constructor(props) {
@@ -11,10 +12,14 @@ class UserStoriesItem extends React.Component {
                 <li id="user-stories-item">
                     <div id="user-stories-border">
                         <div id="user-stories-padding">
-                            <img id="user-stories-icon" src={this.props.user.photoUrl}></img>
+                            <Link className="link" to={`/users/${this.props.user.id}`}>
+                                <img id="user-stories-icon" src={this.props.user.photoUrl}></img>
+                            </Link>
                         </div>
                     </div>
-                    <p id="user-stories-username">{this.props.user.username}</p>
+                    <Link className="link" to={`/users/${this.props.user.id}`}>
+                        <p id="user-stories-username">{this.props.user.username}</p>
+                    </Link>
                 </li>
             </div>
         )
