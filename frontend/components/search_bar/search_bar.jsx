f00b6d7a6fa82cdbaaa380d07b075285e1cee3cb
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-03 02:45:15 -0800

    Search bar not working

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -1,43 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // searchUser: '',
             searchedUsersArray: []
         }
 
         this.handleFilter = this.handleFilter.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleFilter(e) {
         const searchWord = e.currentTarget.value;
         const newFilter = this.props.users.filter((user) => {
-            return user.username.includes(searchWord);
+            return user.username.toLowerCase().includes(searchWord.toLowerCase());
         });
 
-        this.setState({ searchedUsersArray: newFilter });
+        if (searchWord === "") {
+            this.setState({ searchedUsersArray: []})
+        } else {
+            this.setState({ searchedUsersArray: newFilter });
+        }
     }
 
-    // componentDidUpdate(prevProps, prevState) {
-    //     if (prevState.searchUser !== this.state.searchUser && this.state.searchUser !== "") {
+    handleClick() {
+        this.setState({ searchedUsersArray: [] });
+        document.getElementById("search-bar-input").value= "";
+    }
 
-    //     }
-    // }
 
     render() {
+        let display;
+
+        if (this.state.searchedUsersArray.length !== 0) {
+            display = this.state.searchedUsersArray.slice(0,6).map(user => {
+                return (
+                    <Link onClick={this.handleClick} to={`/users/${user.id}`} className="link" key={user.id}>
+                        <div key={user.id} id="data-item">
+                            <img id="search-bar-image" src={user.photoUrl}></img>
+                            <div id="search-bar-usernames-info">
+                                <p id="search-bar-username">{user.username}</p>
+                                <p id="search-bar-name">{user.name}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )
+            })
+        } else if (this.state.searchedUsersArray.length === 0) {
+            display = <div>No users found.</div>
+        }
+
+
         return (
             <div>
                 <div id="search-bar-container">
-                    <svg id="search-icon" aria-label="Search" className="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>
+                    {this.state.searchedUsersArray.length === 0 ?
+                        <svg id="search-icon" aria-label="Search" className="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>
+                    :
+                        <img id="search-icon" src={searchCloseButton} alt="closeButton"></img>
+                    }
                     <input id="search-bar-input" type="text" placeholder="Search" onChange={this.handleFilter}></input>
                 </div>
                 <div id="data-result">
-                    {this.state.searchedUsersArray.length != 0 && (
+                    {this.state.searchedUsersArray.length !== 0 && (
                         <div id="data-result-wrapper">
-                            {this.state.searchedUsersArray.map(user => {
+                            {/* {this.state.searchedUsersArray.slice(0,6).map(user => {
                                 return (
+                                <Link onClick={this.handleClick} to={`/users/${user.id}`} className="link" key={user.id}>
                                     <div key={user.id} id="data-item">
                                         <img id="search-bar-image" src={user.photoUrl}></img>
                                         <div id="search-bar-usernames-info">
@@ -45,8 +76,10 @@ class SearchBar extends React.Component {
                                             <p id="search-bar-name">{user.name}</p>
                                         </div>
                                     </div>
+                                </Link>
                                 )
-                            })}
+                            })} */}
+                            {display}
                         </div>
                     )}
                 </div> 
