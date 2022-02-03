de994a7b860cc114d63c0d28f3ec1696673853d2
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-03 10:14:40 -0800

    Fix flex direction and close button

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -66,7 +66,7 @@ class SearchBar extends React.Component {
         return (
             <div>
                 <div id="search-bar-container">
-                    {this.state.searchedUsersArray.length === 0 ?
+                    {this.state.searchParams.length === 0 ?
                         <svg id="search-icon" aria-label="Search" className="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>
                     :
                         <img id="search-icon" src={searchCloseButton} alt="closeButton"></img>
