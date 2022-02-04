077857d748b0b2adf882e2371b1a705069621507
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-04 09:22:51 -0800

    Remove logger

--- frontend/instacam.jsx
+++ frontend/instacam.jsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
 
-// import { signup, login, logout } from './util/session_api_util'
-// import { fetchAllPosts, fetchPost, createPost, updatePost, deletePost } from "./util/post_api_util";
-import { login, logout, signup } from './actions/session_actions';
-import { fetchAllPosts, fetchPost, createPost, updatePost, deletePost } from "./actions/post_actions";
-import { fetchAllLikes, createLike, deleteLike } from "./actions/like_actions";
-import { fetchAllComments, createComment, deleteComment } from "./actions/comment_actions"
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -28,27 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
         store = configureStore();
     }
 
-    // TESTING START
-    // window.signup = signup;
-    // window.login = login;
-    // window.logout = logout;
-
-    // window.fetchAllPosts = fetchAllPosts;
-    // window.fetchPost = fetchPost;
-    // window.createPost = createPost;
-    // window.updatePost = updatePost;
-    // window.deletePost = deletePost;
-
-    // window.fetchAllLikes = fetchAllLikes;
-    // window.createLike = createLike;
-    // window.deleteLike = deleteLike;
-
-    // window.fetchAllComments = fetchAllComments;
-    // window.createComment = createComment;
-    // window.deleteComment = deleteComment;
-    
-    // window.store = store;
-    // TESTING END
 
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store} />, root);
